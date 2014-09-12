81433f4c2fa905468b49e46e4d2261125a582fd3
Anton <PugachAG@gmail.com>
2014-09-12 18:04:28 +0300

    Add Sustem Programming Lab2

---
+++ SystemProgramming/Lab2/Lab2/Automaton/IAutomaton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Automaton
+{
+    public interface IAutomaton
+    {
+        /// <summary>
+        /// True if automaton accepts input word
+        /// </summary>
+        /// <param name="word">input word</param>
+        /// <returns></returns>
+        bool CheckRecognizable(string word);
+    }
+}
---
+++ SystemProgramming/Lab2/Lab2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
