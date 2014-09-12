29800a39650569321e82a157c74c50c1f7637564
PugachAG <PugachAG@gmail.com>
2014-09-12 21:19:39 +0300

    Update System Programming Lab2.Tests

---
+++ SystemProgramming/Lab2/Lab2.Test/Assets/AssetsPathHepler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Test.Assets
+{
+    public static class AssetsPathHepler
+    {
+        const string AssetsFolerPrefix = "Assets//";
+        const string AssetsTestsSuffix = "Tests";
+        const string AssetsDefinitionsSuffix = "Definition";
+        const string AssetsAutomatonTestsPrefix = "Automaton";
+        const int AssetsAutomatonTestsCount = 1;
+
+        public static string[] GetAssetsAutomatonTestsPaths()
+        {
+            return GenerateFilePaths(AssetsAutomatonTestsPrefix, AssetsTestsSuffix, AssetsAutomatonTestsCount);
+        }
+
+        public static string[] GetAssetsAutomatonDefinitionsPaths()
+        {
+            return GenerateFilePaths(AssetsAutomatonTestsPrefix, AssetsDefinitionsSuffix, AssetsAutomatonTestsCount);
+        }
+
+        private static string[] GenerateFilePaths(string prefix, string suffix, int count, string extension = ".txt")
+        {
+            List<string> result = new List<string>();
+            for (int i = 1; i <= count; i++)
+                result.Add(string.Format("{0}{1}{2}{3}{4}", AssetsFolerPrefix, prefix, i, suffix, extension));
+            return result.ToArray();
+        }
+
+    }
+}
--- SystemProgramming/Lab2/Lab2.Test/Automaton/AutomatonRecognitionTest.cs
+++ SystemProgramming/Lab2/Lab2.Test/Automaton/AutomatonRecognitionTest.cs
@@ -1,4 +1,5 @@
 ﻿using Lab2.Automaton;
+using Lab2.Test.Assets;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
@@ -18,15 +19,30 @@ namespace Lab2.Test.Automaton
         [TestMethod]
         public void TestFiniteStateAutomaton()
         {
-            string[] automatonData = File.ReadAllLines(FiniteAutomatonTestDefinitionPath);
+            string[] automatonDefsPaths = AssetsPathHepler.GetAssetsAutomatonDefinitionsPaths();
+            string[] automatonTestsPaths = AssetsPathHepler.GetAssetsAutomatonTestsPaths();
+            int testsCount = automatonDefsPaths.Length;
+            for (int i = 0; i < testsCount; i++)
+            {
+                IAutomaton automaton = ReadAutomaton(automatonDefsPaths[i]);
+                Dictionary<string, bool> tests = ReadTests(automatonTestsPaths[i]);
+                foreach (var pair in tests)
+                {
+                    bool automatonResult = automaton.CheckRecognizable(pair.Key);
+                    Assert.IsTrue(automatonResult == pair.Value, "Automaton word recognition result should be the same as in predefined tests");
+                }
+            }
 
         }
 
 
-        IAutomaton ReadAutomaton(string fname)
+        private IAutomaton ReadAutomaton(string fname)
         {
-            string[] automatonData = File.ReadAllLines(fname);
+            return null;
+        }
 
+        public Dictionary<string, bool> ReadTests(string fname)
+        {
             return null;
         }
     }
