6ce188df637b4fe9e38f6663e5b2628faf76143c
Anton <PugachAG@gmail.com>
2014-09-12 19:31:12 +0300

    Add System Programming Lab2.Test project

---
+++ SystemProgramming/Lab2/Lab2.Test/Automaton/AutomatonRecognitionTest.cs
@@ -0,0 +1,33 @@
+﻿using Lab2.Automaton;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Test.Automaton
+{
+    [TestClass]
+    public class AutomatonRecognitionTest
+    {
+        const string FiniteAutomatonTestDefinitionPath = "Assets//Automaton1Definition.txt";
+        const string FiniteAutomatonTestTestsPath = "Assets//Automaton1Tests.txt";
+
+        [TestMethod]
+        public void TestFiniteStateAutomaton()
+        {
+            string[] automatonData = File.ReadAllLines(FiniteAutomatonTestDefinitionPath);
+
+        }
+
+
+        IAutomaton ReadAutomaton(string fname)
+        {
+            string[] automatonData = File.ReadAllLines(fname);
+
+            return null;
+        }
+    }
+}
---
+++ SystemProgramming/Lab2/Lab2.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Lab2.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string ss = "Assets//Automaton1Tests.txt";
+            string ff = File.ReadAllText(ss);
+        }
+    }
+}
