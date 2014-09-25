ab86f213daf75eb0ba63d25e3c61be355ba555ff
PugachAG <PugachAG@gmail.com>
2014-09-25 18:25:39 +0300

    Update SystemProgramming Lab2 - add more regex matching tests

--- SystemProgramming/Lab2/Lab2.Test/RegularExpression/RegexMatchTester.cs
+++ SystemProgramming/Lab2/Lab2.Test/RegularExpression/RegexMatchTester.cs
@@ -14,7 +14,7 @@ namespace Lab2.Test.RegularExpression
         }
 
         [TestMethod]
-        public void TestRegexMatch()
+        public void TestRegexMatch1()
         {
             //(a+b)*c
             SingleSymbolRegularExpression regexA = new SingleSymbolRegularExpression(CreateSymbol('a'));
@@ -57,6 +57,54 @@ namespace Lab2.Test.RegularExpression
             foreach (string str in unacceptable)
                 Assert.IsFalse(isMatch(str), str);
             
+        }
+
+        [TestMethod]
+        public void TestRegexMatch2()
+        {
+            //(a+b)*(c+d)*
+            SingleSymbolRegularExpression regexA = new SingleSymbolRegularExpression(CreateSymbol('a'));
+            SingleSymbolRegularExpression regexB = new SingleSymbolRegularExpression(CreateSymbol('b'));
+            SingleSymbolRegularExpression regexC = new SingleSymbolRegularExpression(CreateSymbol('c'));
+            SingleSymbolRegularExpression regexD = new SingleSymbolRegularExpression(CreateSymbol('d'));
+            AlternationRegularExpression unionAB = new AlternationRegularExpression(regexA, regexB);
+            AlternationRegularExpression unionCD = new AlternationRegularExpression(regexC, regexD);
+            KleeneStarRegularExpression starUnionAB = new KleeneStarRegularExpression(unionAB);
+            KleeneStarRegularExpression starUnionCD = new KleeneStarRegularExpression(unionCD);
+            ConcatenationRegularExpression concat = new ConcatenationRegularExpression(starUnionAB, starUnionCD);
+
+            Func<string, bool> isMatch = new Func<string, bool>(str => concat.IsMatch(str));
+
+            string[] acceptable = new string[]
+            {
+                "c",
+                "ac",
+                "abc",
+                "aac",
+                "abac",
+                "abababbabababbbababbabababababababbabababac",
+                "dddddddddddddddddddddddddd",
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac",
+                "abababababaaaaaaaaaaaabababababbbbbabbabababbababababbabababccccccdddcdcddddddcdcdcdcdccccdcdcdcd",
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbccccccccccccccccccccccccccccccccccdddddddddddddcccccccccccccccddddddddddddddddddcccccccccccdcdcdcdcdccccc"
+
+            };
+
+            foreach (string str in acceptable)
+                Assert.IsTrue(isMatch(str), str);
+
+            string[] unacceptable = new string[]
+            {
+                "aca",
+                "abcda",
+                "aaaaaaaaaabbbbbbbbbbccccccccccccccdb",
+                "asadfldjfahsldkfjhasldkjfhsalkdjfhalskdjfhaslkdjfhaslkdjfhlaskdjfhlaskdfhaskljdfhlkc",
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaae"
+            };
+
+            foreach (string str in unacceptable)
+                Assert.IsFalse(isMatch(str), str);
+
         }
     }
 }
