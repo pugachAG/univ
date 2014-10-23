87f9c9658782aaa1f16933df244fb6cdb395d138
PugachAG <pugachag@gmail.com>
2014-10-23 20:15:26 +0300

    Update MathModeling Lab

--- MathModeling/Lab/Lab/App.xaml.cs
+++ MathModeling/Lab/Lab/App.xaml.cs
@@ -13,5 +13,14 @@ namespace Lab
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow window = new MainWindow()
+            {
+                DataContext = new MainViewModel()
+            };
+            window.Show();
+        }
     }
 }
---
+++ MathModeling/Lab/Lab/MainViewModel.cs
@@ -0,0 +1,76 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPMControls.Drawing;
+
+namespace Lab
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ObservableCollection<IFunction> Functions { get; private set; }
+
+        private double tMin = 0;
+        private double tMax = 10;
+        private double tValue = 0;
+        private FuncWrapper<double, double> func;
+
+        public double TMin
+        {
+            get
+            {
+                return tMin;
+            }
+            set
+            {
+                tMin = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public double TMax
+        {
+            get
+            {
+                return tMax;
+            }
+            set
+            {
+                tMax = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public double TValue
+        {
+            get
+            {
+                return tValue;
+            }
+            set
+            {
+                tValue = value;
+                RedrawFunction();
+                RaisePropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            this.Functions = new ObservableCollection<IFunction>();
+            tValue = tMin;
+            func = new FuncWrapper<double, double>(x => Math.Sin(TValue * x) * x + Math.Sin(TValue));
+        }
+
+        private void RedrawFunction()
+        {
+            this.Functions.Clear();
+            this.Functions.Add(this.func);
+        }
+
+    }
+}
